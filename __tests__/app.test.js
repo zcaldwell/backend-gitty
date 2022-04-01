8646b1c5074b6b661a8c681a828eec9cdc4c162a
zcaldwell <mintyst.john@gmail.com>
2022-04-01 09:48:06 -0700

    oauth test done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('backend-gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +12,13 @@ describe('backend-gitty routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should redirect to the github oauth page upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
   });
 });
